3e08705424dfa7d7b21e0aed86730454bcd8f169
Arjun <arjunajith440@gmail.com>
2024-08-31 06:38:21 +0530

    ui

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -8,7 +8,6 @@ const Home = () => {
     return (
     <div id='about1' className='about-container'>   
         <Vision />
-        <MovingText />
         <Card /> 
         <Features />
     </div>
--- src/components/Carousel1.js
+++ src/components/Carousel1.js
@@ -1,36 +1,40 @@
-import './Carousel1.css'
-import img from '../images/pexels-ssteenbergenn-2966315.jpg'
+import './Carousel1.css';
+import img from '../images/pexels-ssteenbergenn-2966315.jpg';
+import React from 'react';
+
 const Carousel1 = () => {
-    return(
+
+    return (
         <div className='carousel-container'>
             <div className='carousel-img'>
-                <img src={img} alt='img'/>
-            </div>
-        
-            <div className="shape-container">
-                <svg className="shape" viewBox="0 0 100 100">
-                    <circle cx="50" cy="50" r="22" fill="white" />
-                    <foreignObject x="10" y="35" width="80" height="80">
-                    <span class="material-symbols--report-off-outline"></span>
-                    </foreignObject>
-                </svg>
-
-                <svg className="shape" viewBox="0 0 100 100">
-                    <circle cx="50" cy="50" r="22" fill="white" />
-                    <text className='text' x="50" y="50" fill="black" textAnchor="middle" alignmentBaseline="middle">Y</text>
-                </svg>
-
-                <svg className="shape" viewBox="0 0 100 100">
-                    <circle cx="50" cy="50" r="22" fill="white" />
-                    <text className='text' x="50" y="50" fill="black" textAnchor="middle" alignmentBaseline="middle">A</text>
-                </svg>
-
-                <svg className="shape" viewBox="0 0 100 100">
-                    <circle cx="50" cy="50" r="22" fill="white" />
-                    <text className='text' x="50" y="50" fill="black" textAnchor="middle" alignmentBaseline="middle">Y</text>
-                </svg>
+                <img src={img} alt='img' />
+                <div className='img-text'>
+                    <div className='img-text-container'>
+                    <h1 className='img-font'>
+                        <span>W</span>
+                        <span>e</span>
+                        <span>l</span>
+                        <span>c</span>
+                        <span>o</span>
+                        <span>m</span>
+                        <span>e</span>
+                        <span style={{ paddingLeft: '5px' }}> </span>
+                        <span>t</span>
+                        <span>o</span>
+                        <span style={{ paddingLeft: '5px' }}> </span>
+                        <span>A</span>
+                        <span>m</span>
+                        <span>p</span>
+                        <span>P</span>
+                        <span>a</span>
+                        <span>y</span>
+                    </h1>
+                    </div>
+                
+                </div>
             </div>
         </div>
-    )
-}
-export default Carousel1
+    );
+};
+
+export default Carousel1;

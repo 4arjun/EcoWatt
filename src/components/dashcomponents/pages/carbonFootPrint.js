44b71d080e6cd14b80cd386e626f79e69a23d1c6
stefan <stefan.binoj.007@gmail.com>
2024-08-31 18:51:06 +0530

    page update

---
+++ src/components/dashcomponents/pages/carbonFootPrint.js
@@ -0,0 +1,12 @@
+import './carbonFootPrint.css'
+import React from 'react';
+
+const CarbonFootPrint =()=>{
+    return(
+        <div className='carbon-foot-bg'>
+            
+        </div>
+
+    )
+}
+export default CarbonFootPrint
